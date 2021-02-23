8a7b2dced44e1b9d04cf9ed1380a8be1c3a180e1
Tijani Hussein <husseintijani2017@gmail.com>
2021-02-23 23:48:51 +0100

    First Commit

---
+++ Gatevest/inebur.com/whmcs/cart98d9.php
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from inebur.com/whmcs/cart.php?gid=registerdomain by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Feb 2021 22:28:46 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=index.php/store/shared-hosting.html?a=add%26domain=register"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="index.php/store/shared-hosting.html?a=add%26domain=register"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from inebur.com/whmcs/cart.php?gid=registerdomain by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Feb 2021 22:28:48 GMT -->
+</HTML>
---
+++ Gatevest/inebur.com/whmcs/index.php/store/cart697f.php
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from inebur.com/whmcs/index.php/store/cart.php?gid=renewals by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Feb 2021 22:28:46 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=shared-hosting.html"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="shared-hosting.html"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from inebur.com/whmcs/index.php/store/cart.php?gid=renewals by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 23 Feb 2021 22:28:46 GMT -->
+</HTML>
